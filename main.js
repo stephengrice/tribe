fd15c31770e9911b9ce662ac10e5dbb60bf2fc8b
Stephen Grice <stephengrice@live.com>
2018-09-09 19:49:31 -0400

    Add functioning Spawn and Command modes

--- main.js
+++ main.js
@@ -5,13 +5,44 @@ var peopleIDs = 0;
 
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
+var controller;
 const FPS = 30;
 const LOOP_INTERVAL = 1000 / FPS;
 const STATE = ['wait', 'walk', 'turn left', 'turn right'];
 const MAX_ACTION_TIME = 2;
+const MODE = {
+  SPAWN: 0,
+  COMMAND: 1,
+};
 ctx = canvas.getContext('2d');
 
 window.onload = function() {
+  // Setup controller
+  controller = new Controller();
+  canvas.onmousedown = function(e) {
+    controller.handleMouseDown(e);
+  }
+  canvas.onmouseup = function(e) {
+    controller.handleMouseUp(e);
+  }
+  canvas.onmousemove = function(e) {
+    controller.handleMouseMove(e);
+  }
+
+  // Setup GUI buttons
+  btnSpawn = document.getElementById('btnSpawn');
+  btnCommand = document.getElementById('btnCommand');
+  btnSpawn.onclick = function() {
+    btnSpawn.disabled = true;
+    btnCommand.disabled = false;
+    controller.setMode(MODE.SPAWN);
+  };
+  btnCommand.onclick = function() {
+    btnSpawn.disabled = false;
+    btnCommand.disabled = true;
+    controller.setMode(MODE.COMMAND);
+  };
+
   fixCanvasSize();
   setInterval(loop, LOOP_INTERVAL);
 }
@@ -25,16 +56,64 @@ function fixCanvasSize() {
   canvas.height = windowHeight;
 }
 
-canvas.onclick = function(e) {
-  people.push(new Person(e.clientX, e.clientY));
-}
-
 function loop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (var i = 0; i < people.length; i++) {
     if (!people[i]) continue;
     people[i].draw();
     people[i].act();
+  }
+  // Draw controller - bounding box
+  controller.draw();
+}
+
+class Controller {
+  constructor() {
+    this.mode = MODE.SPAWN;
+    this.bounding = false;
+    this.boundBegin = {x: 0, y: 0}
+    this.boundEnd = {x: 0, y: 0}
+  }
+  handleMouseDown(e) {
+    if (this.mode == MODE.COMMAND) {
+      this.beginDrag(e);
+    }
+  }
+  handleMouseUp(e) {
+    if (this.mode == MODE.SPAWN) {
+      people.push(new Person(e.clientX, e.clientY));
+    } else if (this.mode == MODE.COMMAND) {
+      this.endDrag(e);
+    }
+  }
+  handleMouseMove(e) {
+    if (this.mode == MODE.COMMAND) {
+      this.boundEnd = {x: e.clientX, y: e.clientY};
+    }
+  }
+  setMode(mode) {
+    this.mode = mode;
+  }
+  beginDrag(e) {
+    this.bounding = true;
+    this.boundBegin = {x: e.clientX, y: e.clientY}
+    this.boundEnd = {x: e.clientX, y: e.clientY}
+  }
+  endDrag(e) {
+    this.bounding = false;
+  }
+  draw() {
+    if (this.bounding) {
+      ctx.fillStyle = "rgba(0,0,0,0.2)";
+      ctx.fillRect(this.boundBegin.x, this.boundBegin.y, this.boundEnd.x - this.boundBegin.x, this.boundEnd.y - this.boundBegin.y)
+      // ctx.beginPath();
+      // ctx.lineTo(this.boundBegin.x, this.boundBegin.y); //b,b
+      // ctx.lineTo(this.boundBegin.x, this.boundEnd.y); // b,e
+      // ctx.lineTo(this.boundEnd.x, this.boundEnd.y); // e,e
+      // ctx.lineTo(this.boundEnd.x, this.boundBegin.y); // e,b
+      // ctx.closePath();
+      // ctx.fillRect(this.boundBegin.x, this.boundBegin.y, 10,10);
+    }
   }
 }
 
