53fe2a776ffb719414da78223f6955a0137effb4
Stephen Grice <stephengrice@live.com>
2018-09-09 01:05:36 -0400

    Add bounds checking and dual direction rotation

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 const FPS = 30;
 const LOOP_INTERVAL = 1000 / FPS;
-const STATE = ['wait', 'walk', 'turn'];
+const STATE = ['wait', 'walk', 'turn left', 'turn right'];
 const MAX_ACTION_TIME = 2;
 ctx = canvas.getContext('2d');
 
@@ -14,6 +14,15 @@ window.onload = function() {
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
   setInterval(loop, LOOP_INTERVAL);
+}
+
+// Fix canvas on window resize
+window.onresize = fixCanvasSize;
+function fixCanvasSize() {
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+  canvas.width = windowWidth;
+  canvas.height = windowHeight;
 }
 
 canvas.onclick = function(e) {
@@ -30,14 +39,14 @@ function loop() {
 
 class Person {
   constructor(x,y) {
-    this.x = x;
-    this.y = y;
-
     this.v = 0;
     this.rot = 0;
 
     this.width = 10;
     this.height = 10;
+
+    this.x = x;
+    this.y = y;
 
     this.state = STATE.WAIT;
     this.chooseNextChange();
@@ -48,22 +57,35 @@ class Person {
         this.v = 0;
         break;
       case 'walk':
+        // console.log('walking' + this.rot)
         this.v = 1;
         break;
-      case 'turn':
+      case 'turn left':
+        // console.log('turning left' + this.rot)
         this.v = 0;
         this.rot += 1;
         break;
-    }
-    if (this.state == 'walk') {
-      this.v = 1;
-    } else if (this.state == 'turn') {
-      this.rot += 1;
+      case 'turn right':
+        // console.log('turning right' + this.rot)
+        this.v = 0;
+        this.rot -= 1;
+        break;
     }
 
     // Move according to vx, vy, and rot
     this.x += this.v * Math.sin(Math.PI * this.rot / 180);
     this.y += this.v * Math.cos(Math.PI * this.rot / 180);
+    // Bounds check
+    if (this.x > WIDTH) {
+      this.x = WIDTH;
+    } else if (this.x < 0) {
+      this.x = 0;
+    }
+    if (this.y > HEIGHT) {
+      this.y = HEIGHT;
+    } else if (this.y < 0) {
+      this.y = 0;
+    }
     // Choose next change of behavior and when
     if (this.nextChange <= new Date().getTime()) {
       this.chooseNextChange();
@@ -72,12 +94,11 @@ class Person {
   }
   draw() {
     ctx.fillStyle = "black";
-    ctx.fillRect(this.x, this.y - this.height / 2, this.width, this.height);
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   chooseNextState() {
     let choice = Math.floor(Math.random() * STATE.length); // Choose a random next state
     this.state = STATE[choice];
-    console.log('next state: ' + STATE[choice]);
   }
   chooseNextChange() {
     let now = new Date().getTime();
